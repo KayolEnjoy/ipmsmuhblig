61d1eb49905cf659b839a9f4013e321e561f5707
ipmsmuhblig <162787556+ipmsmuhblig@users.noreply.github.com>
2024-03-09 16:52:32 +0700

    Update v1.3 main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,43 @@
+function sendEmail() {
+  Swal.fire({
+      icon: 'info',
+      title: 'Mengirim Pesan...',
+      showConfirmButton: false
+  });
+
+  const templateParams = {
+      user_name: document.getElementById('name').value,
+      user_email: document.getElementById('email').value,
+      subject: document.getElementById('subject').value,
+      message: document.getElementById('message').value
+  };
+
+  emailjs.send('service_zrgukwl', 'template_l077dah', templateParams, 'coYun9myjfXn2EY1H')
+      .then(function(response) {
+          // Email terkirim, tampilkan SweetAlert
+          Swal.close();
+
+          Swal.fire({
+              icon: 'success',
+              title: 'Email Terkirim!',
+              text: 'Terima kasih atas pesan Anda.',
+              confirmButtonText: 'Ok'
+          });
+
+          // Reset formulir
+          document.getElementById('contactForm').reset();
+      }, function(error) {
+          // Error dalam mengirim email, tampilkan SweetAlert kesalahan
+          Swal.close();
+
+          Swal.fire({
+              icon: 'error',
+              title: 'Error!',
+              text: 'Gagal mengirim pesan. Silakan coba lagi nanti.',
+              confirmButtonText: 'Ok'
+          });
+      });
+}
 AOS.init({
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
@@ -27,20 +67,12 @@ const quotes = [
       tokoh: "Ali bin Abi Thalib"
   },
   {
-      quote: "Sesungguhnya hati yang paling bersih di antara kamu ialah hati yang paling tulus (ikhlas). Yang paling jauh daripada dosa di antara kamu ialah yang paling tulus.",
-      tokoh: "Rasulullah SAW"
-  },
-  {
       quote: "Jangan menunda-nunda amal kebaikan. Melakukan sedikit kebaikan hari ini lebih baik daripada bermimpi tentang melakukan banyak kebaikan besok.",
       tokoh: "Umar bin Khattab"
   },
   {
     quote: "Tidak ada yang lebih dalam dan lebih dalam dari pengetahuan Allah, yang lebih luas dan lebih luas dari kebijaksanaan Allah. Apa pun yang kamu peroleh, kau akan menemukan lebih banyak lagi, dan seberapa banyak yang kau peroleh, kamu akan melihat bahwa kamu belum mencapai apa pun.",
     tokoh: "Ali bin Abi Thalib"
-  },
-  {
-      quote: "Sesungguhnya hati yang paling bersih di antara kamu ialah hati yang paling tulus (ikhlas). Yang paling jauh daripada dosa di antara kamu ialah yang paling tulus.",
-      tokoh: "Rasulullah SAW"
   },
   {
       quote: "Jangan menunda-nunda amal kebaikan. Melakukan sedikit kebaikan hari ini lebih baik daripada bermimpi tentang melakukan banyak kebaikan besok.",
@@ -163,10 +195,6 @@ const quotes = [
       tokoh: "Abu Bakar Ash-Shiddiq"
   },
   {
-      quote: "Orang yang paling aku cintai adalah orang yang paling shalih di antara kalian, yang terus berbakti pada keluarganya, dan aku adalah orang yang paling shalih di antara kalian terhadap keluargaku.",
-      tokoh: "Muhammad SAW"
-  },
-  {
       quote: "Sungguh benar bahwa harta itu penawar hati yang paling baik. Tetapi penyembuhnya hanyalah bagi hati yang tak tertarik olehnya.",
       tokoh: "Ali bin Abi Thalib"
   },
@@ -257,6 +285,198 @@ const quotes = [
   {
       quote: "Ketika kita merasa lemah, ingatlah bahwa Allah adalah tempat kita bersandar dan bermohon pertolongan.",
       tokoh: "Ustadz Hanan Attaki"
+  },
+  {
+    quote: "Keikhlasan adalah kunci untuk meraih kebahagiaan sejati.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Berbuat baiklah kepada sesama tanpa mengharapkan balasan dari mereka.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Kesabaran adalah pelita yang menerangi jalan di tengah kegelapan.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Hiduplah sesuai dengan ajaran agama, dan kamu akan meraih kedamaian batin.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Jadilah orang yang selalu bersyukur atas segala nikmat yang diberikan Allah.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Tetaplah teguh pada prinsipmu meskipun orang lain tidak memahaminya.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Hiduplah dengan rendah hati, karena rendah hati adalah kunci kesuksesan.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Janganlah kamu menyia-nyiakan waktu, karena waktu adalah karunia terbesar dari Allah.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Setiap kesulitan pasti ada jalan keluarnya, percayalah pada kekuatan Allah.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Kebaikan yang kita lakukan akan kembali kepada kita dengan berlipat ganda.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Janganlah menyalahkan orang lain atas kesalahan yang kita buat, belajarlah untuk bertanggung jawab.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Ketika kita mencintai Allah, kita akan mencintai sesama makhluk-Nya.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Tetaplah berdoa meskipun segala sesuatunya terasa sulit, karena doa adalah kuncinya.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Hidup ini singkat, jangan sia-siakan waktu dengan hal yang tidak bermanfaat.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Sesungguhnya dalam kesabaran itu ada kekuatan yang besar.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Janganlah terlalu terpaku pada dunia, ingatlah bahwa akhiratlah yang sebenarnya.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Tetaplah optimis meskipun dalam keadaan sulit, karena optimisme adalah kunci kesuksesan.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Berkat kesabaranlah seseorang bisa meraih kesuksesan yang diinginkannya.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Janganlah kita menyerah begitu saja, percayalah bahwa Allah selalu menyertai kita.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Hiduplah dengan penuh keikhlasan, karena keikhlasan akan membawa kita kepada kebahagiaan.",
+    tokoh: "Ustadz Adi Hidayat"
+  },
+  {
+    quote: "Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu pasangan-pasangan dari jenismu sendiri, supaya kamu cenderung dan merasa tenteram kepadanya, dan dijadikan-Nya di antaramu rasa kasih dan sayang. Sesungguhnya pada yang demikian itu benar-benar terdapat tanda-tanda bagi kaum yang berpikir.",
+    tokoh: "Al-Qur'an (Ar-Rum: 21)"
+  },
+  {
+    quote: "Dan jika kamu menghitung nikmat Allah, niscaya kamu tidak dapat menghitungnya. Sesungguhnya Allah benar-benar Maha Pengampun lagi Maha Penyayang.",
+    tokoh: "Al-Qur'an (An-Nahl: 18)"
+  },
+  {
+    quote: "Sesungguhnya Allah tidak akan mengubah keadaan suatu kaum sehingga mereka mengubah keadaan yang ada pada diri mereka sendiri.",
+    tokoh: "Al-Qur'an (Ar-Ra'd: 11)"
+  },
+  {
+    quote: "Dan berpeganglah kamu semuanya kepada tali (agama) Allah, dan janganlah kamu bercerai berai. Dan ingatlah akan nikmat Allah kepadamu ketika kamu dahulu (masa jahiliyah) bermusuh-musuhan, maka Allah mempersatukan hatimu, lalu menjadilah kamu karena nikmat Allah, orang-orang yang bersaudara; dan kamu telah berada di tepi jurang neraka, lalu Allah menyelamatkan kamu dari padanya. Demikianlah Allah menjelaskan ayat-ayat-Nya kepadamu, agar kamu mendapat petunjuk.",
+    tokoh: "Al-Qur'an (Ali Imran: 103)"
+  },
+  {
+    quote: "Orang mukmin yang paling sempurna imannya adalah yang paling baik akhlaknya. Dan sebaik-baik kalian adalah yang baik terhadap wanita.",
+    tokoh: "Hadis Riwayat Tirmidzi"
+  },
+  {
+    quote: "Barangsiapa yang beriman kepada Allah dan hari akhir, hendaklah dia berkata yang baik atau diam.",
+    tokoh: "Hadis Riwayat Bukhari dan Muslim"
+  },
+  {
+    quote: "Sesungguhnya di dalam tubuh ada segumpal daging, jika baik maka baik pula seluruh tubuh, dan jika rusak maka rusak pula seluruh tubuh. Ketahuilah, itulah hati.",
+    tokoh: "Hadis Riwayat Bukhari dan Muslim"
+  },
+  {
+    quote: "Orang yang paling aku cintai di antara kalian adalah yang paling baik akhlaknya.",
+    tokoh: "Hadis Riwayat Bukhari dan Muslim"
+  },
+  {
+    quote: "Dan sesungguhnya, kamu benar-benar akan diberi pahala yang tidak terhingga.",
+    tokoh: "Al-Qur'an (Az-Zumar: 10)"
+  },
+  {
+    quote: "Sesungguhnya, bersama kesulitan ada kemudahan.",
+    tokoh: "Al-Qur'an (Al-Insyirah: 5)"
+  },
+  {
+    quote: "Sesungguhnya, setiap amal (perbuatan) itu bergantung pada niatnya.",
+    tokoh: "Hadis Riwayat Bukhari dan Muslim"
+  },
+  {
+    quote: "Barangsiapa yang menutup aib seorang muslim, maka Allah akan menutup aibnya di dunia dan akhirat.",
+    tokoh: "Hadis Riwayat Muslim"
+  },
+  {
+    quote: "Sesungguhnya Allah tidak akan mengubah keadaan suatu kaum sehingga mereka mengubah keadaan yang ada pada diri mereka sendiri.",
+    tokoh: "Al-Qur'an (Ar-Ra'd: 11)"
+  },
+  {
+    quote: "Sesungguhnya, Allah bersama orang yang sabar.",
+    tokoh: "Al-Qur'an (Al-Baqarah: 153)"
+  },
+  {
+    quote: "Jika kamu bersyukur, pasti Kami akan menambah (nikmat) kepadamu, dan jika kamu mengingkari (nikmat-Ku), maka sesungguhnya azab-Ku sangat pedih.",
+    tokoh: "Al-Qur'an (Ibrahim: 7)"
+  },
+  {
+    quote: "Orang yang paling dicintai oleh Allah adalah orang yang paling bermanfaat bagi orang lain.",
+    tokoh: "Hadis Riwayat Ahmad"
+  },
+  {
+    quote: "Berpeganglah kamu semuanya kepada tali (agama) Allah, dan janganlah kamu bercerai berai.",
+    tokoh: "Al-Qur'an (Ali Imran: 103)"
+  },
+  { 
+    quote: "Barangsiapa yang beriman kepada Allah dan hari akhir, hendaklah dia berkata yang baik atau diam.",
+    tokoh: "Hadis Riwayat Bukhari dan Muslim"
+  },
+  {
+    quote: "Dan janganlah kamu berjalan di muka bumi ini dengan sombong. Sesungguhnya kamu sekali-kali tidak dapat menembus bumi dan sekali-kali kamu tidak akan sampai setinggi gunung.",
+    tokoh: "Al-Qur'an (Al-Isra: 37)"
+  },
+  {
+    quote: "Sesungguhnya, dengan kesulitan ada kemudahan.",
+    tokoh: "Al-Qur'an (Asy-Syarh: 5-6)"
+  },
+  {
+    quote: "Orang mukmin yang paling sempurna imannya adalah yang paling baik akhlaknya.",
+    tokoh: "Hadis Riwayat Tirmidzi"
+  },
+  {
+    quote: "Barangsiapa yang menunjukkan suatu kebaikan, maka dia akan mendapatkan pahala seperti orang yang melakukannya.",
+    tokoh: "Hadis Riwayat Muslim"
+  },
+  {
+    quote: "Dan hendaklah kamu berlaku baik kepada orang tua kedua ibu-bapamu.",
+    tokoh: "Al-Qur'an (Al-Isra: 23)"
+  },
+  {
+    quote: "Sesungguhnya, setiap perkataan itu ada kebohongannya, kecuali tiga perkara.",
+    tokoh: "Hadis Riwayat Abu Daud"
+  },
+  {
+    quote: "Dan janganlah kamu memakan harta sesamamu dengan jalan yang batil.",
+    tokoh: "Al-Qur'an (Al-Baqarah: 188)"
+  },
+  {
+    quote: "Janganlah kamu memandang sepele suatu kebaikan, walaupun itu bertemu dengan saudaramu dengan wajah yang senang.",
+    tokoh: "Hadis Riwayat Muslim"
+  },
+  {
+    quote: "Katakanlah kepada orang-orang yang beriman: \"Hendaklah mereka menahan pandangannya dan memelihara kemaluannya; yang demikian itu adalah lebih suci bagi mereka. Sesungguhnya Allah Maha Mengetahui apa yang mereka perbuat.",
+    tokoh: "Al-Qur'an (An-Nur: 30)"
+  },
+  {
+    quote: "Tidak akan masuk surga orang yang memutuskan silaturahmi.",
+    tokoh: "Hadis Riwayat Muslim"
   }
 ];
 
